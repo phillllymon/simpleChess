6f1a4f44c196a274d08aa802675ab19f1c7372ad
Parker Harris <rparkerharris@gmail.com>
2024-01-08 10:47:57 -0800

    Add skeleton for explore tab

--- helpers/activateControls.js
+++ helpers/activateControls.js
@@ -2,21 +2,36 @@ import { computerTakeTurn, updateBoard } from "./board.js";
 
 export function activateTabControls() {
     const playTab = document.getElementById("play_tab");
-    const apiTab = document.getElementById("api_tab");
+    const apiGuideTab = document.getElementById("api_guide_tab");
+    const exploreApiTab = document.getElementById("explore_api_tab");
     const playSection = document.getElementById("play_section");
-    const apiSection = document.getElementById("api_section");
-    apiTab.addEventListener("click", () => {
-        playSection.classList.add("hidden");
-        apiSection.classList.remove("hidden");
-        playTab.classList.add("inactive_tab");
-        apiTab.classList.remove("inactive_tab");
-    });
+    const apiGuideSection = document.getElementById("api_guide_section");
+    const exploreApiSection = document.getElementById("explore_api_section");
     playTab.addEventListener("click", () => {
         playSection.classList.remove("hidden");
-        apiSection.classList.add("hidden");
+        apiGuideSection.classList.add("hidden");
+        exploreApiSection.classList.add("hidden");
         playTab.classList.remove("inactive_tab");
-        apiTab.classList.add("inactive_tab");
+        apiGuideTab.classList.add("inactive_tab");
+        exploreApiTab.classList.add("inactive_tab");
     });
+    apiGuideTab.addEventListener("click", () => {
+        playSection.classList.add("hidden");
+        apiGuideSection.classList.remove("hidden");
+        exploreApiSection.classList.add("hidden");
+        playTab.classList.add("inactive_tab");
+        apiGuideTab.classList.remove("inactive_tab");
+        exploreApiTab.classList.add("inactive_tab");
+    });
+    exploreApiTab.addEventListener("click", () => {
+        playSection.classList.add("hidden");
+        apiGuideSection.classList.add("hidden");
+        exploreApiSection.classList.remove("hidden");
+        playTab.classList.add("inactive_tab");
+        apiGuideTab.classList.add("inactive_tab");
+        exploreApiTab.classList.remove("inactive_tab");
+    });
+    
 }
 
 export function activateBoardControls() {
